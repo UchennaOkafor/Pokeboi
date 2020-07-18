7522de0a56e9690f30edce4e3711fe461f78aa32
Uchenna Okafor <22923123+UchennaOkafor@users.noreply.github.com>
2020-07-18 19:54:49 +0100

    * Added bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import jQuery from 'jquery'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+global.jQuery = jQuery
+global .$ = jQuery
+
 Vue.config.productionTip = false
 
 new Vue({
